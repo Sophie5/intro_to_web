25b79e9d30f1acd3339ef9a228c383014e18d30a
Sophie <sophie.k.mclean@btinternet.com>
2017-02-28 22:44:24 +0000

    cat border

--- app.rb
+++ app.rb
@@ -1,9 +1,6 @@
 require "sinatra"
 
 # set:session_secret, 'super secret
-
 get '/cat' do
-  "<div>
-  <img style='border: 3px dashed red' src='http://bit.ly/1eze8aE'>
-   </div>"
-end
+erb(:index)
+end 
